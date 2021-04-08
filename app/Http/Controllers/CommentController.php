c37b8eb47d495382cb0e9ebabbe426440cbc25ad
qudi <shintarou0327@yahoo.co.jp>
2021-04-08 17:32:51 +0900

    commit

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -12,29 +12,26 @@ class CommentController extends Controller
 {
     public function comment(Request $request){
         $now = Carbon::now();
-        $param = [
-            'user_id' => $request->user_id,
-            'movie_id' => $request->movie_id,
-            'content' => $request->content,
-            'created_at' => $now,
-            'updated_at' => $now,
-        ];
-        DB::table('comments')->insert($param);
-        return response()->json([
-            'message' => 'posted successfully',
-            'data' => $param
-        ],200);
+        $item = Comment::where('user_id',$request->user_id)->first();
+        if($item){
+            $item->content = $request->content;
+            $item->save();
+        }   else{
+            $param = [
+                'user_id' => $request->user_id,
+                'movie_id' => $request->movie_id,
+                'content' => $request->content,
+                'created_at' => $now,
+                'updated_at' => $now,
+            ];
+            DB::table('comments')->insert($param);
+            return response()->json([
+                'message' => 'posted successfully',
+                'data' => $param
+            ], 200);
+        }
     }
-    public function put(Request $request)
-    {
-        $param = [
-            'content' => $request->content
-        ];
-        $item = Comment::where('id', $request->id)->update($param);
-        return response()->json([
-            'message' => 'updated successfully'
-        ], 200);
-    }
+    
     public function show($id)
     {
         $item = Comment::where('id', $id)->first();
